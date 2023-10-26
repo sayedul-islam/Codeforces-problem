f3357b950789bc257aab2e05e20360769c5de0cc
Md Sayedul Islam <56690290+sayedul-islam@users.noreply.github.com>
2023-10-26 08:54:36 +0600

    Add files via upload
    Codeforces [959A - Mahmoud and Ehab and the even-odd game]

---
+++ odd&evengame.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+  long long n;
+  cin>>n;
+  n=n-2;
+  if(n%2==0)
+    cout<<"Mahmoud"<<endl;
+  else
+    cout<<"Ehab"<<endl;
+return 0;
+}
+
+
+
