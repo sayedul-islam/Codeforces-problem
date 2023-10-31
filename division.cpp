4fc258a62a5a529477238251e3f4a32b13175abc
Md Sayedul Islam <56690290+sayedul-islam@users.noreply.github.com>
2023-10-31 12:35:12 +0600

    1669A - Division?

---
+++ division.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll             long long
+#define fast           ios_base::sync_with_stdio(false); cin.tie(NULL)
+using namespace std;
+int main(){
+    fast;
+    int t;
+    cin>>t;
+    while(t--){
+        int rating;
+        cin>>rating;
+        if(rating>=1900) cout<<"Division 1"<<endl;
+        else if(rating>=1600 && rating<=1899) cout<<"Division 2"<<endl;
+        else if(rating>=1400 && rating<=1599) cout<<"Division 3"<<endl;
+        else {
+            if(rating<=1399) cout<<"Division 4"<<endl;
+        }
+    }
+   return 0;
+}
+
