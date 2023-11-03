134feb464fc6977389911cf4ce2f70133b7cfcf8
Md Sayedul Islam <56690290+sayedul-islam@users.noreply.github.com>
2023-11-03 12:13:31 +0600

    50A - Domino piling

---
+++ dominopilling.cpp
@@ -0,0 +1,14 @@
+
+#include<bits/stdc++.h>
+#define ll             long long
+#define fast           ios_base::sync_with_stdio(false); cin.tie(NULL)
+using namespace std;
+int main() {
+       int n,m;
+       cin>>n>>m;
+       cout<<(n*m)/2<<endl;
+
+
+    return 0;
+}
+
