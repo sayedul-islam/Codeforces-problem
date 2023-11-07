8ab7c5419e7e0a8384c57fc044c25d8f7ffdc400
Md Sayedul Islam <56690290+sayedul-islam@users.noreply.github.com>
2023-11-07 19:41:26 +0600

    281A - Word Capitalization

---
+++ Word Capitalization.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define ll             long long
+#define fast           ios_base::sync_with_stdio(false); cin.tie(NULL)
+using namespace std;
+int main(){
+    fast;
+   string s;
+   cin>>s;
+   s[0]=toupper(s[0]);
+   cout<<s<<endl;
+
+   return 0;
+}
+
